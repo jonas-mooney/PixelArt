5c31a032f6c2cbbeada89ae744d3bc06a2113be7
Jonas <jonasmooney2@gmail.com>
2025-08-29 11:09:33 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SideNav from './sideNav/sideNav';
+
+function App() {
+  return (
+    <div className="app">
+      <SideNav />
+      <h3>testing</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ src/sideNav/sideNav.tsx
@@ -0,0 +1,13 @@
+
+
+function sideNav() {
+    return (
+        <div className="sideNav">
+            <span>nav item</span>
+            <span>nav item</span>
+            <span>nav item</span>
+        </div>
+    )
+}
+
+export default sideNav; 
