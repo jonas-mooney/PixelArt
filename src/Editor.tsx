4f5feb6fa9157ee6c7fde48d31fe76afb02a3160
Jonas <jonasmooney2@gmail.com>
2025-09-09 20:18:05 -0600

    Zoom and pan library

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,3 +1,21 @@
+import {
+  TransformWrapper,
+  TransformComponent,
+  useControls,
+} from "react-zoom-pan-pinch";
+
+const Controls = () => {
+  const { zoomIn, zoomOut, resetTransform } = useControls();
+
+  return (
+    <div className="tools">
+      <button onClick={() => zoomIn()}>+</button>
+      <button onClick={() => zoomOut()}>-</button>
+      <button onClick={() => resetTransform()}>x</button>
+    </div>
+  );
+};
+
 function Editor() {
   let cubeArray = [];
 
@@ -7,20 +25,39 @@ function Editor() {
       .toString(16)
       .padStart(6, "0");
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 10000; i++) {
     cubeArray.push({ i, color: randomHexColor() });
   }
 
   return (
-    <div className="editor">
-      {cubeArray.map((cube, index) => (
-        <div
-          key={index}
-          className="cube"
-          style={{ backgroundColor: cube.color }}
-        ></div>
-      ))}
-    </div>
+    // <div className="editor-container">
+    <TransformWrapper
+      limitToBounds={true}
+      alignmentAnimation={{ sizeX: 0, sizeY: 0 }}
+      centerZoomedOut={true}
+      minScale={0.5}
+      maxScale={4}
+      initialScale={1}
+      initialPositionX={0}
+      initialPositionY={0}
+    >
+      {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+        <>
+          <Controls />
+          <TransformComponent>
+            <div className="editor">
+              {cubeArray.map((cube, index) => (
+                <div
+                  key={index}
+                  className="cube"
+                  style={{ backgroundColor: cube.color }}
+                ></div>
+              ))}
+            </div>
+          </TransformComponent>
+        </>
+      )}
+    </TransformWrapper>
   );
 }
 
