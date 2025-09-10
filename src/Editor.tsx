eced4d46005272fd5279aec4b3a215719e6d34e9
Jonas <jonasmooney2@gmail.com>
2025-09-09 18:26:26 -0600

    Colored squares

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,20 +1,27 @@
-
-
 function Editor() {
+  let cubeArray = [];
 
-    let cubeArray = [];
+  const randomHexColor = () =>
+    "#" +
+    Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, "0");
 
-    for (let i = 0; i < 1000; i++) {
-        cubeArray.push({i, color: '#861a1aff'});
-    }
+  for (let i = 0; i < 1000; i++) {
+    cubeArray.push({ i, color: randomHexColor() });
+  }
 
-    return (
-        <div className="main-components editor">
-            {cubeArray.map((cube, index) => (
-                <div key={index} className="cube" style={{backgroundColor: cube.color}}></div>
-            ))}
-        </div>
-    )
+  return (
+    <div className="editor">
+      {cubeArray.map((cube, index) => (
+        <div
+          key={index}
+          className="cube"
+          style={{ backgroundColor: cube.color }}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
 export default Editor;
